688cbaae662c96166cf59b0f97c71020f5cba116
Vikesh Chauhan <vikeshppk@gmail.com>
2025-01-25 15:00:21 +0530

    add firstName and lastName

--- backend/testing/src/main/java/com/security/testing/entity/User.java
+++ backend/testing/src/main/java/com/security/testing/entity/User.java
@@ -20,6 +20,14 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+    
+    @Column(nullable = false)
+    private String firstName;
+    
+    @Column(nullable = false)
+    private String lastName;
+    
+    
 
     // Getters and setters
 
@@ -46,5 +54,27 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	
+	
+		
+    
+    
 }
 
